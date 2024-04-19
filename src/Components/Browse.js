66ad98dc93b5a1403b139ee7d897aad60b3af372
Varun Wadhwa <vwadhwa3@gmail.com>
2024-04-19 11:57:09 +0530

    toggle

--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Browse = () => {
+      const [isActive,setIsActive] = useState(true)
+      const toggleActiveButton = () => {
+        setIsActive(!isActive);
+      };
   return (
-    <div>Browse</div>
+    <div className='absolute top-[50%] left-[50%]'> 
+        <button className={isActive?"bg-green-700 w-20 h-16 rounded-md":"bg-red-700 w-20 h-16 rounded-md"} onClick={toggleActiveButton}> 1 </button>
+        <button className={!isActive?"bg-green-700 w-20 h-16 rounded-md":"bg-red-700 w-20 h-16 rounded-md"} onClick={toggleActiveButton}> 2 </button>       
+    </div>
   )
   
 }
